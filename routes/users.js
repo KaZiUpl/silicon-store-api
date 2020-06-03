8c84f521031a711d930c14595805d321eb4ebfcc
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-06-03 13:40:31 +0200

    added user registration and login

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,114 @@
 var express = require('express');
 var mysql = require('../config/mysql');
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 var router = express.Router();
 
 // user register
 router.post('/', function (req, res) {
-  res.json('user created');
+  // check whether email is taken
+  mysql.query(
+    'SELECT email FROM users where email = ?',
+    [req.body.email],
+    (err, fields, rows) => {
+      if (err) {
+        res.sendStatus(500);
+        return;
+      } else {
+        if (fields.length > 0) {
+          res.status(400).json({
+            message: 'Mail taken',
+          });
+          return;
+        }
+        // create password hash and add new user to database
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            res.sendStatus(500);
+            return;
+          } else {
+            // create new user
+            mysql.query(
+              'INSERT INTO users(email, name, password, role) VALUES(?,?,?,?)',
+              [req.body.email, req.body.name, hash, 'Customer'],
+              (err, rows, fields) => {
+                if (err) {
+                  res.sendStatus(500);
+                  return;
+                } else {
+                  res.status(201).json({
+                    message: 'Customer created',
+                  });
+                  return;
+                }
+              }
+            );
+          }
+        });
+      }
+    }
+  );
 });
 
 // user login
-router.post('/key', function (req, res) {
-  res.json('user logged in');
+router.post('/token', function (req, res) {
+  mysql.query(
+    'SELECT id,email, password, role from users WHERE email = ?',
+    [req.body.email],
+    (err, rows, fields) => {
+      // user doesn't exists
+      if (rows.length < 1) {
+        res.status(401).json({
+          message: 'Bad credentials',
+        });
+        return;
+      } else {
+        user = rows[0];
+        console.log(user);
+
+        // compare passwords
+        bcrypt.compare(req.body.password, user.password, (err, result) => {
+          if (err) {
+            res.status(401).json({
+              message: 'Bad credentials',
+            });
+            return;
+          } else {
+            //success login
+            if (result) {
+              const token = jwt.sign(
+                {
+                  email: user.email,
+                  id: user.id,
+                  role: user.role,
+                },
+                process.env.JWT_SECRET,
+                {
+                  expiresIn: '1h',
+                }
+              );
+              res.status(200).json({
+                token: token,
+                id: user.id,
+                mail: user.email
+              });
+            } else {
+              res.status(401).json({
+                message: 'Bad credentials',
+              });
+            }
+          }
+        });
+      }
+    }
+  );
 });
 
 // get user profile
 router.get('/:id', function (req, res) {
   mysql.query(
-    'SELECT id, email, name FROM users WHERE id=' + req.params.id,
+    'SELECT id, email, name FROM users WHERE id = ?',
+    [req.params.id],
     (err, rows, fields) => {
       res.json(rows);
     }
@@ -25,7 +118,8 @@ router.get('/:id', function (req, res) {
 // getting order list
 router.get('/:id/orders', function (req, res) {
   mysql.query(
-    'SELECT * from orders WHERE user_id = ' + req.params.id,
+    'SELECT * from orders WHERE user_id = ?',
+    [req.params.id],
     (err, rows, fields) => {
       res.json(rows);
     }
@@ -34,7 +128,13 @@ router.get('/:id/orders', function (req, res) {
 
 //get shopping cart
 router.get('/:id/cart', function (req, res) {
-  res.json('your shopping cart');
+  mysql.query(
+    'SELECT * FROM cart_items WHERE user_id = ?',
+    [req.params.id],
+    (err, rows, fields) => {
+      res.json(rows);
+    }
+  );
 });
 
 module.exports = router;
