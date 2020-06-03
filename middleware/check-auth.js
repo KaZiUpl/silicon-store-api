83fa36f3e3807aed723bf1955b088f75a69c54ba
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-06-03 21:14:25 +0200

    added user role guard

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,14 +1,13 @@
 var jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.userData = decoded;
-        
-        next();
-    }
-    catch(error) {
-        return res.sendStatus(403);
-    }
-}
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userData = decoded;
+
+    next();
+  } catch (error) {
+    return res.sendStatus(403);
+  }
+};
---
+++ middleware/check-role.js
@@ -0,0 +1,13 @@
+/**
+ * Middleware for checking whether user's role is the same as provided.
+ * This middleware is used in pair with check-auth middleware.
+ */
+module.exports = function (requiredRole) {
+  return function (req, res, next) {
+    userRole = req.headers.userData.role;
+    if (userRole != requiredRole) {
+      return res.status(403).json({ message: 'Forbidden' });
+    }
+    next();
+  };
+};
