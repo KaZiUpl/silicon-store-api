ccfdb85ad75ba5ffe72a0f678fa1d9b8f94030ae
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-06-04 21:21:16 +0200

    Update comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,17 @@ var mysql = require('../config/mysql');
 var router = express.Router();
 
 router.post('/', function (req, res) {
-  res.json('comment added');
+  mysql.query(
+    'INSERT INTO comments(item_id, user_id, text) VALUES(?,?,?)',
+    [req.body.item_id, req.userData.id, req.body.text],
+    (err, rows, fields) => {
+      if (err) {
+        return res.sendStatus(500);
+      } else {
+        return res.sendStatus(201);
+      }
+    }
+  );
 });
 
 module.exports = router;
