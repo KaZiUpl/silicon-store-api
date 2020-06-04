d24215b6e332e1716b57e7aa14bbbfcaadc2176d
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-06-04 21:23:29 +0200

    Update categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -4,51 +4,53 @@ var router = express.Router();
 
 // get all categories
 router.get('/', function (req, res) {
-  if (req.query.category) {
-    mysql.query(
-      'SELECT * FROM categories WHERE id =' + req.query.category,
-      (err, rows, fields) => {
-        res.json(rows);
-      }
-    );
-  } else {
-    mysql.query('SELECT * FROM categories', (err, rows, fields) => {
-      res.json(rows);
-    });
-  }
+  mysql.query('SELECT * FROM categories', (err, rows, fields) => {
+    if (err) {
+      return res.sendStatus(500);
+    }
+    return res.status(200).json(rows);
+  });
 });
+
 // get main categories
 router.get('/main-categories', function (req, res) {
   mysql.query(
     'SELECT * FROM categories WHERE parent_id IS NULL',
     (err, rows, fields) => {
-      res.json(rows);
+      if (err) {
+        return res.sendStatus(500);
+      }
+      return res.status(200).json(rows);
     }
   );
 });
+
 // get category
 router.get('/:id', function (req, res) {
   mysql.query(
     'SELECT * FROM categories WHERE id=' + req.params.id,
     (err, rows, fields) => {
-      if (rows.length > 0) {
-        res.json(rows[0]);
+      if (err) {
+        return res.sendStatus(500);
+      }
+      if (rows.length == 0) {
+        return res.status(200).json(rows[0]);
       } else {
-        res.sendStatus(404);
+        return res.sendStatus(404);
       }
     }
   );
 });
+
 // get category's children
 router.get('/:id/child-categories', function (req, res) {
   mysql.query(
     'SELECT * FROM categories WHERE parent_id=' + req.params.id,
     (err, rows, fields) => {
-      if (rows.length > 0) {
-        res.json(rows);
-      } else {
-        res.sendStatus(404);
+      if (err) {
+        return res.sendStatus(500);
       }
+      return res.status(200).json(rows);
     }
   );
 });
