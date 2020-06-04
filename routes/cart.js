9f472f68b860dcceb5b60dc54dff56baa8281b70
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-06-04 21:21:33 +0200

    Update cart.js

--- routes/cart.js
+++ routes/cart.js
@@ -1,28 +1,97 @@
 var express = require('express');
 var mysql = require('../config/mysql');
+var checkAuth = require('../middleware/check-auth');
 var router = express.Router();
 
-router.use(function (req, res, next) {
-  next();
+router.use(checkAuth);
+
+// update cart
+router.patch('/', function (req, res) {
+  // check if item is already in the cart
+  mysql.query(
+    'SELECT * from cart_items WHERE user_id = ? AND item_id = ?',
+    [req.userData.id, req.body.item_id],
+    (err, rows, fields) => {
+      if (err) {
+        res.sendStatus(500);
+      }
+      // if item is in the cart
+      if (rows.length > 0) {
+        let newAmount = req.body.amount;
+        if (newAmount < 1) {
+          return res.sendStatus(400);
+        }
+        // update cart item
+        mysql.query(
+          'UPDATE cart_items SET amount = ? WHERE user_id = ? AND item_id = ?',
+          [newAmount, req.userData.id, req.body.item_id],
+          (err, rows, fields) => {
+            if (err) {
+              return res.sendStatus(500);
+            }
+            return res.sendStatus(200);
+          }
+        );
+      } else {
+        // add item to the cart
+        mysql.query(
+          'INSERT INTO cart_items(user_id, item_id, amount) VALUES(?,?,?)',
+          [req.userData.id, req.body.item_id, 1],
+          (err, rows, fields) => {
+            if (err) {
+              return res.sendStatus(500);
+            }
+            return res.sendStatus(201);
+          }
+        );
+      }
+    }
+  );
 });
 
-// add item to cart
-router.post('/', function (req, res) {
-  res.json('added item to cart');
+// get cart
+router.get('/', function (req, res) {
+  mysql.query(
+    'SELECT * FROM cart_items WHERE user_id = ?',
+    [req.userData.id],
+    (err, rows, fields) => {
+      if(err)  {
+        return res.sendStatus(500)
+      }
+      return res.status(200).json(rows);
+    }
+  );
 });
 
 // get cart item
-router.get('/:id', function (req, res) {
-  res.json('Got item ' + req.params.id);
+router.get('/:item_id', function (req, res) {
+  mysql.query(
+    'SELECT * FROM cart_items WHERE user_id = ? AND item_id = ?',
+    [req.userData.id, req.params.item_id],
+    (err, rows, fields) => {
+      if (err) {
+        return res.sendStatus(500);
+      }
+      if (rows.length == 0) {
+        return res.sendStatus(404);
+      }
+      return res.status(200).json(rows[0]);
+    }
+  );
 });
 
-// modify cart item
-router.put('/:id', function (req, res) {
-  res.json('Modified cart item ' + req.params.id);
-});
-
-router.delete('/:id', function (req, res) {
-  res.json('Deleted cart item ' + req.params.id);
+// delete cart item
+router.delete('/:item_id', function (req, res) {
+  mysql.query(
+    'DELETE FROM cart_items WHERE user_id = ? AND item_id = ?',
+    [req.userData.id, req.params.item_id],
+    (err, rows, fields) => {
+      if (err) {
+        return res.sendStatus(500);
+      }
+      return res.sendStatus(200);
+    }
+  );
 });
 
 module.exports = router;
