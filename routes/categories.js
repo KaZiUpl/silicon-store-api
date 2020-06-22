43f2e04ae2aff8006590fa8df099ca07b8c70a55
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-06-22 13:41:19 +0200

    Update categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -11,7 +11,7 @@ router.get('/main-categories', CategoriesController.getMainCategories);
 // get category
 router.get('/:id', CategoriesController.getCategory);
 // get category's breadcrumbs
-router.get(':id/breadcrumbs', CategoriesController.getCategoryBreadcrumbs);
+router.get('/:id/breadcrumbs', CategoriesController.getCategoryBreadcrumbs);
 // get category's children categories
 router.get('/:id/child-categories', CategoriesController.getChildrenCategories);
 
