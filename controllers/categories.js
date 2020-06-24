a3f866556f32a4ddc8b8ab2e0807a5eb14a1ae5a
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-06-24 22:57:45 +0200

    Update categories.js

--- controllers/categories.js
+++ controllers/categories.js
@@ -14,37 +14,40 @@ exports.getAllCategories = async function (req, res) {
 exports.getMainCategories = async function (req, res) {
   try {
     let mainCategories = await mysql.query(
-    'SELECT * FROM categories WHERE parent_id IS NULL');
+      'SELECT * FROM categories WHERE parent_id IS NULL'
+    );
 
     return res.status(200).json(mainCategories);
   } catch (error) {
     res.sendStatus(500);
-    throw err;
+    throw error;
   }
 };
 
 exports.getChildrenCategories = async function (req, res) {
   try {
     let childrenCategories = await mysql.query(
-    'SELECT * FROM categories WHERE parent_id=' + req.params.id);
+      'SELECT * FROM categories WHERE parent_id=' + req.params.id
+    );
 
     return res.status(200).json(childrenCategories);
   } catch (error) {
     res.sendStatus(500);
-    throw err;
+    throw error;
   }
 };
 
 exports.getCategoryBreadcrumbs = async function (req, res) {
   try {
-    let categoryBreadcrumbs = mysql.query(
-    'SELECT * FROM categories WHERE FIND_IN_SET(categories.id, (SELECT REPLACE((SELECT categories.path FROM categories WHERE categories.id = ?), "/",",")))',
-    req.params.id);
+    let categoryBreadcrumbs = await mysql.query(
+      'SELECT * FROM categories WHERE FIND_IN_SET(categories.id, (SELECT REPLACE((SELECT categories.path FROM categories WHERE categories.id = ?), "/",",")))',
+      req.params.id
+    );
 
     return res.status(200).json(categoryBreadcrumbs);
   } catch (error) {
     res.sendStatus(500);
-    throw err;
+    throw error;
   }
 };
 
@@ -52,15 +55,16 @@ exports.getCategory = async function (req, res) {
   try {
     let category = await mysql.query(
       'SELECT * FROM categories WHERE id = ?',
-      req.params.id);
+      req.params.id
+    );
 
-      if(category.length == 0) {
-        return res.sendStatus(404);
-      }
+    if (category.length == 0) {
+      return res.sendStatus(404);
+    }
 
-      return res.status(200).json(category);
+    return res.status(200).json(category);
   } catch (error) {
     res.sendStatus(500);
-    throw err;
+    throw error;
   }
 };
